ddf25d3c5777eced0823d7208c27705ef9f1b5d0
iamshs <mdsholayman998@gmail.com>
2022-04-04 04:25:39 +0600

    homepage-img-done

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -6,7 +6,8 @@ const Home = () => {
     return (
         <div className='product-container'>
             <div className="product-detail">
-              <h2>laptop</h2>
+              <h1 >Buying the <span className='product-title'>BEST ONE!!</span></h1>
+              <p>Now available with an OLED screen, an 8TB SSD, and Intel "Tiger Lake-H" processing, the Dell XPS 15 is our top pick among premium desktop-replacement laptops.</p>
             </div>
             <div className="product-img">
                 <img src={image} alt="" />
